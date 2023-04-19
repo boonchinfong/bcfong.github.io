6faae37c0d46f0ad9e07d3cd5f0c58f3bde640cb
Fong Boon Chin <Chin@Fongs-MacBook-Air-2.local>
2023-04-19 20:35:25 +0700

    Add existing file

--- script.js
+++ script.js
@@ -22,4 +22,13 @@ document.getElementById('submitBtn').addEventListener('click', () => {
       }
 })
 
+// Animated Text
+
+const text = document.querySelector(".text p")
+text.innerHTML = text.innerText.split("").map((char, i) => 
+  `<span style="transform:rotate(${i*9.2}deg)">${char}</span>`
+).join("")
+
+
+
 
